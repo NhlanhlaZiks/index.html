bc0b45c5edd61c500b774e0a417c16e6df047fc1
NhlanhlaZiks <125467381+NhlanhlaZiks@users.noreply.github.com>
2024-11-04 14:18:17 +0200

    changed the h2 to a p and made a few changes

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -55,3 +55,8 @@ export default function WeatherAppApp() {
     return form;
   }
 }
+
+<p>
+  This project was coded by <a href="#"> Nhlanhla Zikalala</a> and is hosted on{" "}
+  <a href="https://github.com/NhlanhlaZiks/index.html"> Github</a>
+</p>;
--- src/index.js
+++ src/index.js
@@ -10,10 +10,5 @@ root.render(
   <div className="WeatherApp">
     <h1>Weather App</h1>
     <WeatherApp />
-    <h2>
-      This project was coded by
-      <a href="#">Nhlanhla Zikalala</a> and is hosted on
-      <a href="https://github.com/NhlanhlaZiks/index.html">Github</a>
-    </h2>
   </div>
 );
